f693fdbcd93e14acccd9ca779e1b1d72f4474e12
Sarah Iraurgui <iraurguisj@gmail.com>
2021-04-02 18:35:06 -0400

    G$ passed to global object

--- Greetr.js
+++ Greetr.js
@@ -4,6 +4,8 @@
     var Greetr = function (firstName, lastName, language) {
         return new Greetr.init(firstName, lastName, language);
     }
+    Greetr.prototype = {}; 
+
     Greetr.init = function(firstName, lastName, language) {
 
         var self = this;
@@ -12,7 +14,10 @@
         self.language = language || "en";
         
     }
-    
+Greetr.init.prototype = Greetr.prototype;
+
+ global.Greetr = global.G$ = Greetr;
+
 }(window, $));
 
 
