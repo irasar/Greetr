170579ff7d15268e5dc20483f9dc518d93d92de7
Sarah Iraurgui <iraurguisj@gmail.com>
2021-04-02 20:10:46 -0400

    library complete! now lets use it

--- Greetr.js
+++ Greetr.js
@@ -1,44 +1,51 @@
 
 (function (global, $) {
-
+    // "new" an object
     var Greetr = function (firstName, lastName, language) {
         return new Greetr.init(firstName, lastName, language);
     }
-
+    //hidden within the scope of the IIFE and never directly accessible 
     var supportedLangs = ["en", "es"];
-
+    //informal greetings
     var greetings = {
         en: "hello",
         es: "hola"
     };
-
+    // formal greetings
     var formalGreetings = {
         en: "greetings",
         es: "saludos"
     };
-
+    // logger messages
     var logMessages = {
         en: "logged in",
         es: "inicio sesion"
     };
-
+    // prototype holds methods (to save memory space)
     Greetr.prototype = {
+
+        // "this" refers to the calling object at execution time
         fullName: function () {
             return this.firstName + " " + this.lastName;
         },
         validate: function () {
+            // check that is a valid language
+            // refrences the externally inaccessible "supportedLangs" within the closure
             if (supportedLangs.indexOf(this.language) === -1) {
                 throw "Invalid language";
             }
         },
+        // retrieve messages from objet by referring to properties using [] syntax
         greeting: function () {
             return greetings[this.language] + " " + this.firstName + "!";
         },
         formalGreeting: function () {
-            return formalGreetings[this.language] + "," + this.fullName();
+            return formalGreetings[this.language] + ", " + this.fullName();
         },
+        // chainable methods return their own containing object
         greet: function (formal) {
             var msg;
+            // if undefined or null it will be coerced to "false"
 
             if (formal) {
                 msg = this.formalGreeting();
@@ -54,22 +61,46 @@
             return this;
         },
 
-        log: function() {
+        log: function () {
             if (console) {
-                console.log(logMessages[this.language] + ": " + 
-                this.fullName());
+                console.log(logMessages[this.language] + ": " +
+                    this.fullName());
             }
+            //make chainable 
             return this;
         },
-        setLang: function(lang) {
+        setLang: function (lang) {
+            // set the language
             this.language = lang;
-
+            // valdate
             this.validate();
+            // make chainable
+            return this;
+        },
+
+        htmlGreeting: function (selector, formal) {
+            if (!$) {
+                throw "jquery not loaded";
+            }
+            if (!selector) {
+                throw " missing jquery selector";
+            }
+            // determine the message
+            var msg;
+            if (formal) {
+                msg = this.formalGreeting();
+            }
+            else {
+                msg = this.greeting();
+            }
+            // inject the message in the chosen place in the DOM
+            $(selector).html(msg);
+            // make chainable
             return this;
         }
 
     };
-
+    // The actual object is created here, allowing us to "new" an object without calling "new"
     Greetr.init = function (firstName, lastName, language) {
 
         var self = this;
@@ -77,9 +108,11 @@
         self.lastName = lastName || "";
         self.language = language || "en";
 
+        self.validate();
     }
+    // trick borrowed from jquery so we dont have to use the "new" keyword
     Greetr.init.prototype = Greetr.prototype;
-
+    // attach our greetr to the global object, and provide a shorthand "$G" to ease our poor fingers
     global.Greetr = global.G$ = Greetr;
 
 }(window, $));
--- app.js
+++ app.js
@@ -1,2 +1,2 @@
-var g = G$("john", "doe");
-console.log(g);
+var g = G$("john" , "doe");
+g.greet().setLang("es").greet(true);
